a0795a57ef520fae1e3835f449cacc0aff7cd211
alex <adlitsov@gmail.com>
2018-03-23 15:35:40 +0300

    MainFrame: added code for loading/saving books from/to file (action listeners for openMenuItem, saveMenuItem)

--- src/main/java/com/edu_netcracker/nn/adlitsov/ui/homework1/MainFrame.java
+++ src/main/java/com/edu_netcracker/nn/adlitsov/ui/homework1/MainFrame.java
@@ -4,13 +4,16 @@ import ca.odell.glazedlists.swing.AutoCompleteSupport;
 import com.github.lgooddatepicker.components.DatePicker;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.KeyEvent;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class MainFrame extends JFrame {
     public static final int WIDTH = 800, HEIGHT = 520;
@@ -42,25 +45,68 @@ public class MainFrame extends JFrame {
     private void createMenuBar() {
         JMenuBar menuBar;
         JMenu fileMenu;
-        JMenuItem openMenutItem, saveMenuItem;
+        JMenuItem openMenuItem, saveMenuItem;
 
         menuBar = new JMenuBar();
 
         fileMenu = new JMenu("File");
         menuBar.add(fileMenu);
 
-        openMenutItem = new JMenuItem("Open");
-        openMenutItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
+        openMenuItem = new JMenuItem("Open");
+        openMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
 
-        fileMenu.add(openMenutItem);
+        openMenuItem.addActionListener(event -> {
+            JFileChooser fc = new JFileChooser();
+            FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                    "JSON files", "json");
+            fc.setFileFilter(filter);
+            fc.setAcceptAllFileFilterUsed(false);
+
+            int returnVal = fc.showOpenDialog(this);
+
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File file = fc.getSelectedFile();
+
+                bookModel.loadBooks(file);
+                System.out.println("Saving: " + file.getName());
+            } else {
+                System.out.println("Save command cancelled by user.");
+            }
+
+        });
+
+        fileMenu.add(openMenuItem);
 
         saveMenuItem = new JMenuItem("Save");
         saveMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+
+        saveMenuItem.addActionListener(event -> {
+            JFileChooser fc = new JFileChooser();
+            FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                    "JSON files", "json");
+            fc.setFileFilter(filter);
+            fc.setAcceptAllFileFilterUsed(false);
+
+            int returnVal = fc.showSaveDialog(this);
+
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                String fileName = fc.getSelectedFile().getAbsolutePath();
+                String fileNameWithOutExt = fileName.replaceFirst("[.][^.]+$", "");
+                File file = new File(fileNameWithOutExt + "." + filter.getExtensions()[0]);
+
+                bookModel.saveBooks(file);
+                System.out.println("Saving: " + file.getName());
+            } else {
+                System.out.println("Save command cancelled by user.");
+            }
+
+        });
 
         fileMenu.add(saveMenuItem);
 
         setJMenuBar(menuBar);
     }
+
 
     private void createSplitPane() {
         JTabbedPane tabbedPane = createTabbedPaneForBookOperations();
